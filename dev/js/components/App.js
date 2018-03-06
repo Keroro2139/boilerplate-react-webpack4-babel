b95dc9537215f505855f48841a4f460856d11c78
Keroro2139 <k.chulaket@gmail.com>
2018-03-06 14:16:16 +0700

    git add *

---
+++ dev/js/components/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+const App = (props) => {
+    return (
+        <div>
+            <h1>Welcome to car rent page.</h1>
+        </div>
+    )
+}
+
+export default App;
