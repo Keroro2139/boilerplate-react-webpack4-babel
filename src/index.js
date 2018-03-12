ea6a48cf89a736c757b81da1e2189a5a1c47eb18
Keroro2139 <k.chulaket@gmail.com>
2018-03-12 16:22:56 +0700

    update redux-logger v.3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import promise from 'redux-promise';
+import logger from 'redux-logger';
+
+import allReducers from './reducers';
+import App from './components/App';
+
+const store = createStore(
+    allReducers,
+    applyMiddleware(thunk, promise, logger)
+);
 
 ReactDOM.render(
-    <div>Weerapat Chulaket</div>,
+    <Provider store={store}>
+        <App />
+    </Provider>,
     document.querySelector('#root')
 )
